2ac09eb839f388953b0f1ca23c873009fd067aee
MetalPoutineQc <115269943+MetalPoutineQc@users.noreply.github.com>
2024-07-17 12:58:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
 
-cp "cinnamon (config)" ~/.config/cinnamon -r
-cp "cinnamon (local)" ~/.local/share/cinnamon -r
-cp Pictures ~/Pictures -r
+cp -r "cinnamon (config)" ~/.config/cinnamon
+cp -r "cinnamon (local)" ~/.local/share/cinnamon
+cp -r Pictures ~/Pictures
