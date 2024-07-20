16afe04a4122f9605df48abc2097334de846632a
MetalPoutineQc <metal.poutine@gmail.com>
2024-07-20 10:53:33 -0400

    fix install mangohud

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cp -r "cinnamon (config)" ~/.config/cinnamon
-cp -r "cinnamon (local)" ~/.local/share/cinnamon
-dconf load /org/cinnamon/ < cinnamon-desktop.conf
-dconf load /org/cinnamon/desktop/peripherals/mouse/ < cinnamon-peripheral.conf
+
 apt update && apt upgrade -y
 apt install steam -y
 apt install gamemode -y
@@ -14,3 +11,10 @@ sudo gunzip MangoHud-*.tar.gz && sudo tar -xvf MangoHud-*.tar
 cd /usr/src/Mangohud
 sudo ./mangohud-setup.sh install
 echo "Installation terminée"
+
+cd /opt/GamingOS
+pwd
+cp -r "cinnamon (config)" ~/.config/cinnamon
+cp -r "cinnamon (local)" ~/.local/share/cinnamon
+dconf load /org/cinnamon/ < cinnamon-desktop.conf
+dconf load /org/cinnamon/desktop/peripherals/mouse/ < cinnamon-peripheral.conf
