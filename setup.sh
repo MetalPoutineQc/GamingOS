15fd223e81cd9574d4eac28778fe436f9029ddf4
MetalPoutineQc <metal.poutine@gmail.com>
2024-07-20 10:49:12 -0400

    fix install mangohud

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ flatpak install --app com.github.tchx84.Flatseal com.github.Matoking.protontrick
 cd /usr/src
 sudo wget https://github.com/flightlessmango/MangoHud/releases/download/v0.7.2/MangoHud-0.7.2.r0.g7b80f73.tar.gz
 sudo gunzip MangoHud-*.tar.gz && sudo tar -xvf MangoHud-*.tar
-cd Mangohud
+cd /usr/src/Mangohud
 sudo ./mangohud-setup.sh install
 echo "Installation terminée"
