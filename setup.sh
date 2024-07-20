00d940476fd85dad291afe3df1c115d3535c0549
MetalPoutineQc <metal.poutine@gmail.com>
2024-07-20 11:45:28 -0400

    Modification du script d'installation

--- setup.sh
+++ setup.sh
@@ -5,9 +5,13 @@ apt update && apt upgrade -y
 apt install steam -y
 apt install gamemode -y
 flatpak install --app com.github.tchx84.Flatseal com.github.Matoking.protontricks net.davidotek.pupgui2 com.usebottles.bottles com.discordapp.Discord -y
+cd /opt
 sudo wget https://github.com/flightlessmango/MangoHud/releases/download/v0.7.2/MangoHud-0.7.2.r0.g7b80f73.tar.gz
 sudo gunzip MangoHud-*.tar.gz && sudo tar -xvf MangoHud-*.tar
-sudo MangoHud/./mangohud-setup.sh install
+sudo rm MangoHud-*.tar.gz
+sudo chown -R $USER MangoHud/
+cd MangoHud/
+./mangohud-setup.sh install
 echo "Installation terminée"
 cp -r "cinnamon (config)" ~/.config/cinnamon
 cp -r "cinnamon (local)" ~/.local/share/cinnamon
