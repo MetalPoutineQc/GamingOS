71586c25c2a0e99940b3875b8751f3fbc0465bff
MetalPoutineQc <metal.poutine@gmail.com>
2024-07-20 12:22:31 -0400

    Modification du script setup

--- setup.sh
+++ setup.sh
@@ -14,8 +14,8 @@ cd MangoHud/
 ./mangohud-setup.sh install
 
 cd /opt/GamingOS
-cp -r "cinnamon (config)" ~/.config/cinnamon
-cp -r "cinnamon (local)" ~/.local/share/cinnamon
+cp -r "cinnamon (config)"/* ~/.config/cinnamon
+cp -r "cinnamon (local)"/* ~/.local/share/cinnamon
 dconf load /org/cinnamon/ < cinnamon-desktop.conf
 dconf load /org/cinnamon/desktop/peripherals/mouse/ < cinnamon-peripheral.conf
 
